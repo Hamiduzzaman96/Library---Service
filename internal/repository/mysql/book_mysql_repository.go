f0049b08ae18a7895240507c3f9242dd787591be
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2025-12-28 15:01:37 +0600

    Get method added in repository

--- internal/repository/mysql/book_mysql_repository.go
+++ internal/repository/mysql/book_mysql_repository.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/Hamiduzzaman96/Library---Service/internal/domain"
 )
@@ -18,14 +19,30 @@ func NewBookMySQlRepository(db *sql.DB) *BookMySQLRepository {
 func (r *BookMySQLRepository) Create(ctx context.Context, book *domain.Book) error {
 	query := `INSERT INTO books (title,author,isbn,available) VALUES (?,?,?,?)`
 
-	result, err := r.db.ExecContext(ctx, query, book.Title, book.Author, book.ISBN, book.Available)
+	result, err := r.db.ExecContext(ctx, query, book.Title, book.Author, book.ISBN, book.Available) //ExecContext used for INSER,UPDATE and DELETE
 
 	if err != nil {
 		return err
 	}
-	id, _ := result.LastInsertId()
+	id, _ := result.LastInsertId() //auto increment id return
 	book.ID = id
 
 	return nil
 
+}
+
+func (r *BookMySQLRepository) GetByID(ctx context.Context, id int64) (*domain.Book, error) {
+	query := `SELECT id,title,author,isbn,available FROM books WHERE id = ?`
+	book := &domain.Book{}
+
+	err := r.db.QueryRowContext(ctx, query, id).Scan(
+		&book.ID, &book.Title, &book.Author, &book.ISBN, &book.Available,
+	)
+	if err == sql.ErrNoRows { //database/sql package predefined error,that means query is ok but no rows
+		return nil, errors.New("BOOK NOT FOUND") //custom,readable error, 404
+	}
+	if err != nil {
+		return nil, err
+	}
+	return book, nil
 }
