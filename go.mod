d6756f6f17b23f18245ad553a277cd4b762b38d1
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2025-12-28 16:51:39 +0600

    handler created

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/Hamiduzzaman96/Library---Service
 
 go 1.25.5
+
+require (
+	go.opentelemetry.io/otel v1.39.0
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251222181119-0a764e51fe1b
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+)
---
+++ internal/handler/grpc/book_handler.go
@@ -0,0 +1,79 @@
+package grpc
+
+import (
+	"context"
+
+	pb "github.com/Hamiduzzaman96/Library---Service/Library---Service/proto/bookpb"
+	"github.com/Hamiduzzaman96/Library---Service/internal/domain"
+	"github.com/Hamiduzzaman96/Library---Service/internal/usecase"
+	"go.opentelemetry.io/otel/codes"
+	"google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type BookHandler struct {
+	usecase *usecase.BookUsecase
+	pb.UnimplementedBookServiceServer
+}
+
+func NewBookHandler(u *usecase.BookUsecase) *BookHandler {
+	return &BookHandler{usecase: u}
+}
+
+func (h *BookHandler) CreateBook(ctx context.Context, req *pb.CreateBookRequest) (*pb.Empty, error) {
+	book := &domain.Book{
+		Title:     req.Title,
+		Author:    req.Author,
+		ISBN:      req.Isbn,
+		Available: true,
+	}
+
+	err := h.usecase.Create(ctx, book)
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &pb.Empty{}, nil
+}
+
+func (h *BookHandler) GetBook(ctx context.Context, req *pb.GetBookRequest) (*pb.BookResponse, error) {
+	book, err := h.usecase.GetByID(ctx, req.Id)
+
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &pb.BookResponse{
+		Id:        book.ID,
+		Title:     book.Title,
+		Author:    book.Author,
+		Isbn:      book.ISBN,
+		Available: book.Available,
+	}, nil
+}
+
+func (h *BookHandler) UpdateBook(ctx context.Context, req *pb.UpdateBookRequest) (*pb.Empty, error) {
+	book := &domain.Book{
+		ID:        req.Id,
+		Title:     req.Title,
+		Author:    req.Author,
+		Available: req.Available,
+	}
+
+	err := h.usecase.Update(ctx, book)
+
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+	return &pb.Empty{}, nil
+}
+
+func (h *BookHandler) DeleteBook(ctx context.Context, req *pb.DeleteBookRequest) (*pb.Empty, error) {
+	err := h.usecase.Delete(ctx, req.Id)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &pb.Empty{}, nil
+}
